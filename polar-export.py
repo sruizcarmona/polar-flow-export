110eb2abc6fb77ee2888c384dcc5f6cb22f86ddd
sruizcarmona <sruizcarmona@gmail.com>
2024-07-03 15:46:53 +1000

    fixed polar-export

--- polar-export.py
+++ polar-export.py
@@ -9,9 +9,10 @@ FLOW_URL = "https://flow.polar.com"
 
 def login(driver, username, password):
     driver.get("%s/login" % FLOW_URL)
-    driver.find_element_by_name("email").send_keys(username)
-    driver.find_element_by_name("password").send_keys(password)
-    driver.find_element_by_id("login").click()
+    time.sleep(1)
+    driver.find_element("name", "email").send_keys(username)
+    driver.find_element("name", "password").send_keys(password)
+    driver.find_element("id", "login").click()
 
 def get_exercise_ids(driver, year, month):
     driver.get("%s/diary/%s/month/%s" % (FLOW_URL, year, month))
@@ -19,7 +20,7 @@ def get_exercise_ids(driver, year, month):
     ids = map(
         # The subscript removes the prefix
         lambda e: e.get_attribute("href")[len("https://flow.polar.com/training/analysis/"):],
-        driver.find_elements_by_xpath("//div[@class='event event-month exercise']/a")
+        driver.find_elements("xpath","//div[@class='event event-month exercise']/a")
     )
     return ids
 
@@ -38,6 +39,13 @@ def export_exercise(driver, exercise_id, output_dir):
     r = s.get("%s/api/export/training/tcx/%s" % (FLOW_URL, exercise_id))
     tcx_data = r.text
     filename = _get_filename(r)
+
+    # check whether the specified path exists or not
+    out_exists = os.path.exists(output_dir)
+    if not out_exists:
+        #create a new directory because it does not exist
+        os.makedirs(output_dir)
+        print("Created directory %s" % output_dir)
 
     outfile = open(os.path.join(output_dir, filename), 'w')
     outfile.write(tcx_data)
