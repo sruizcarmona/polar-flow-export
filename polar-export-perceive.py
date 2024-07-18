6002245d48f97251c250c52ae0008d1d474f40ca
sruizcarmona <sruizcarmona@gmail.com>
2024-07-18 15:15:24 +1000

    updated for perceive

--- polar-export-perceive.py
+++ polar-export-perceive.py
@@ -4,13 +4,15 @@ import re
 import time
 import sys
 import os
+from datetime import datetime
+import csv
 
 FLOW_URL = "https://flow.polar.com"
 
 def login(driver, username, password):
     driver.get("%s/login" % FLOW_URL)
     # sleep for 1 second to make sure the page is loaded
-    time.sleep(1)
+    time.sleep(5)
     driver.find_element("name", "email").send_keys(username)
     driver.find_element("name", "password").send_keys(password)
     driver.find_element("id", "login").click()
@@ -21,7 +23,8 @@ def get_exercise_ids(driver, year, month):
     ids = map(
         # The subscript removes the prefix
         lambda e: e.get_attribute("href")[len("https://flow.polar.com/training/analysis/"):],
-        driver.find_elements("xpath","//div[@class='event event-month exercise']/a")
+        driver.find_elements("xpath","//div[@class='event event-month exercise']/a") +
+        driver.find_elements("xpath","//div[@class='event event-month exercise event-item--has-target']/a")
     )
     return ids
 
@@ -52,47 +55,65 @@ def export_exercise(driver, exercise_id, output_dir):
         tcx_data = r.text
         try:
             filename = _get_filename(r)
-            # check whether the specified path exists or not
-            out_exists = os.path.exists(output_dir)
-            if not out_exists:
+            # make directory the first time an activity is found
+            if not os.path.exists(output_dir):
                 #create a new directory because it does not exist
                 os.makedirs(output_dir)
                 print("Created directory %s" % output_dir)
             outfile_path = os.path.join(output_dir, filename)
             with open(outfile_path, 'w') as outfile:
                 outfile.write(tcx_data)
-            print("Wrote file %s" % filename)
+            print("Wrote file %s" % outfile_path)
         except KeyError as e:
             print(e)
     else:
         print("Failed to download file. Status code: %s" % r.status_code)
 
 
-def run(driver, username, password, year, output_dir):
+def run(driver, username, password, output_dir):
     login(driver, username, password)
 
     time.sleep(5)
-    # loop through all months in year
-    for month in range(1, 13):
-        exercise_ids = get_exercise_ids(driver, year, month)
-        for ex_id in exercise_ids:
-            export_exercise(driver, ex_id, output_dir)
+    # loop through years from 2022 to today
+    current_year = datetime.now().year
+    for year in range(2022, current_year + 1):
+        # loop through all months in year
+        for month in range(1, 13):
+            exercise_ids = get_exercise_ids(driver, year, month)
+            for ex_id in exercise_ids:
+                export_exercise(driver, ex_id, output_dir)
 
 if __name__ == "__main__":
     try:
-        (pwdfile, year) = sys.argv[1:]
+        (pwdfile, outdir) = sys.argv[1:]
     except ValueError:
-        sys.stderr.write(("Usage: %s <csvfile> <year>\n") % sys.argv[0])
+        sys.stderr.write(("Usage: %s <csvfile> <outdir>\n") % sys.argv[0])
         sys.exit(1)
 
     # get usernames and passwords from a csv file
-    with open(pwdfile) as f:
-        # read csv and remove newline characters
-        lines = f.read().splitlines()
+with open(pwdfile) as f:
+    # read csv and remove newline characters
+    # lines = f.read().splitlines()
+    reader = csv.reader(f)
+    lines = [[c.replace('\ufeff', '') for c in row] for row in reader]
     for line in lines:
-        username, password, userid = line.split(',')
+        # username, password, userid = line.split(',')
+        username, password, userid = line
+        output_dir = outdir + '/' + userid
+        # check if output_dir exists, if it does, skip user, else create the directory
+        if os.path.exists(output_dir):
+            print("Directory %s already exists, skipping participant %s" % (output_dir, userid))
+            continue
+        else:
+            #create a new directory because it does not exist
+            # os.makedirs(output_dir)
+            print("New participant: %s" % userid)
+
+        # run chrome
         driver = webdriver.Chrome()
         try:
-            run(driver, username, password, year, userid)
+            run(driver, username, password, output_dir)
         finally:
+            # print user finished
+            print("Finished user %s" % userid)
             driver.quit()
