801a7cbb0423f4d755fd9553fec29a2459826a93
sruizcarmona <sruizcarmona@gmail.com>
2024-07-23 20:15:01 +1000

    read csv from polarflow and add sport as suffix

--- polar-export-perceive.py
+++ polar-export-perceive.py
@@ -6,6 +6,7 @@ import sys
 import os
 from datetime import datetime
 import csv
+import pandas
 
 FLOW_URL = "https://flow.polar.com"
 
@@ -49,21 +50,52 @@ def export_exercise(driver, exercise_id, output_dir):
         else:
             # Provide a default filename or raise an error
             raise KeyError("Response does not contain a 'Content-Disposition' header.")
+    
+    # # function to add sport to tcx file after reading csv
+    # # not needed in the end, csv can be read on the fly
+    # def _add_sport_to_tcx(tcx_file, csv_file):
+    #     # read csv file, only the headers and the first row
+    #     df = pandas.read_csv(csv_file, nrows=1)
+    #     # get value for column 'Sport'
+    #     sport = df['Sport'][0]
+    #     # change name of tcx to include sport
+    #     new_tcx = tcx_file.replace('.TCX', '_SPORT_%s.TCX' % sport)
+    #     # change file name of tcx file
+    #     os.rename(tcx_file, new_tcx)
+    #     # remove csv file
+    #     os.remove(csv_file)
 
     r = s.get("%s/api/export/training/tcx/%s" % (FLOW_URL, exercise_id))
+    rcsv = s.get("%s/api/export/training/csv/%s" % (FLOW_URL, exercise_id))
     if r.status_code == 200:
+        # get the tcx data
         tcx_data = r.text
+        # get the csv data
+        csv_data = rcsv.text
+        # get sport from csv_data
+        # csv_data is a raw csv file, split by lines, take the second line (first are headers)
+        # then split by comma and take the second value (SPORT)
+        mysport = csv_data.splitlines()[1].split(',')[1]
+
         try:
+            # get the filename from the response headers for tcx and csv
             filename = _get_filename(r)
+            # filenamecsv = _get_filename(rcsv)
             # make directory the first time an activity is found
             if not os.path.exists(output_dir):
                 #create a new directory because it does not exist
                 os.makedirs(output_dir)
                 print("Created directory %s" % output_dir)
-            outfile_path = os.path.join(output_dir, filename)
+            # write tcx file
+            outfile_path_tmp = os.path.join(output_dir, filename)
+            # add string to tcx file to include sport
+            outfile_path = outfile_path_tmp.replace('.TCX', '_SPORT_%s.TCX' % mysport)
             with open(outfile_path, 'w') as outfile:
                 outfile.write(tcx_data)
             print("Wrote file %s" % outfile_path)
+            # fix tcx file name to include sport
+            # not needed in the end
+            # _add_sport_to_tcx(outfile_path, outfilecsv_path)
         except KeyError as e:
             print(e)
     else:
@@ -106,10 +138,10 @@ with open(pwdfile) as f:
             continue
         else:
             #create a new directory because it does not exist
-            # os.makedirs(output_dir)
+            os.makedirs(output_dir)
             print("New participant: %s" % userid)
 
-        # run chrome
+        # run chrome        
         driver = webdriver.Chrome()
         try:
             run(driver, username, password, output_dir)
